6d4952205e3b7a1d4df55a2bde6ee215bc53803f
Shakil Ahmed Khan <shakil.gd13@gmail.com>
2021-07-31 05:01:04 +0600

    third commit

--- project_shakil/app_shakil/models.py
+++ project_shakil/app_shakil/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
+import datetime
+import os
+
+fs = FileSystemStorage(location='/media/photos')
+
+def filepath(request, filename):
+    old_file = filename
+    timenow = datetime.datetime.now().strftime('%Y%m%d%H:%M:%S')
+    filename = "%s%s" %(timenow, old_file)
+    return os.path.join('uploads/', filename)
 
 # Create your models here.
 class Users(models.Model):
@@ -8,6 +19,9 @@ class Users(models.Model):
     password = models.CharField(max_length=200)
 
 class Blog(models.Model):
-    user = models.ForeignKey(Users, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    body = models.CharField(max_length=2000)
+    blog_image = models.ImageField(upload_to=filepath, null=True, blank=True)
+    blog_title = models.CharField(max_length=200)
+    blog_body = models.CharField(max_length=2000)
+    blog_dateTime = models.DateTimeField(null=True)
+    blog_type = models.CharField(max_length=100, null=True)
+    
--- project_shakil/app_shakil/views.py
+++ project_shakil/app_shakil/views.py
@@ -1,3 +1,4 @@
+from .models import Blog
 from django.shortcuts import render
 
 def index(request):
@@ -10,4 +11,6 @@ def work(request):
     return render(request, 'app_shakil/work.html')
 
 def blog(request):
-    return render(request, 'app_shakil/blog.html')
+    latest_blog_data = Blog.objects.order_by('-blog_dateTime')
+    context = {'latest_blog_data': latest_blog_data,}
+    return render(request, 'app_shakil/blog.html', context)
