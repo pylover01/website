c805388b8a72abaa144c0f93d6cf019e67e8b590
Shakil Ahmed Khan <shakil.gd13@gmail.com>
2021-08-01 18:49:55 +0600

    sharebutton updated and frontend updated

--- project_shakil/project_shakil/settings.py
+++ project_shakil/project_shakil/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app_shakil.apps.AppShakilConfig',
+    'django_social_share',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
